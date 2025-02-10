78c6f82535f7682046a6b174099bdd3e24b53cd0
comsenseuw <40339723+comsenseuw@users.noreply.github.com>
2025-02-10 15:02:34 +0700

    Updated

--- run.bat
+++ run.bat
@@ -11,14 +11,14 @@ set dtf=%date:~-4%-%date:~-7,2%-%date:~-10,2% %time:~0,2%:%time:~3,2%:%time:~6,2
 
 rem Run the Python script with logging
 echo -----------------------------------------------------------------
-echo START U810x checking
+echo START cid
 
 for /f "delims=" %%A in ('py cid.py') do (
     echo [%dtf%] %%A >> "%LOG_FILE%"
     echo %%A
 )
 echo -----------------------------------------------------------------
-echo START Cleaning
+echo START housekeeping
 
 for /f "delims=" %%A in ('py housekeeping.py') do (
     echo [%dtf%] %%A >> "%LOG_FILE%"
