8d8d8950b4ea73b2cc9381496b89a6e3ad124d97
comsenseuw <40339723+comsenseuw@users.noreply.github.com>
2025-02-10 15:06:40 +0700

    Updated

--- houskeeping.py
+++ houskeeping.py
@@ -1,19 +1,19 @@
 import os
 import time
 
-folder_path = '//bthsdv004.infineon.com/PTE_Project/AUTOMATION/000.AUTO_EXTRACTION/BAKE_IN_LOT'
-extracted_path = '//bthsdv004.infineon.com/PTE_Project/AUTOMATION/000.AUTO_EXTRACTION/BAKE_IN_LOT/EBS_LOT_LOG'
+folder_path = '//servername/AUTOMATION/FOLDER'
+extracted_path = '//servername/AUTOMATION/FOLDER/SUBFOLDER'
 current_time = time.time()
 
 for file_name in os.listdir(folder_path):
-    if file_name.startswith('BAKEIN_LOT_MVIN'):
+    if file_name.startswith('filename'):
         file_path = os.path.join(folder_path, file_name)
         file_modified_time = os.path.getmtime(file_path)
         time_difference = current_time - file_modified_time
         time_difference_days = time_difference / (24 * 60 * 60)
         if time_difference_days > 7:
             os.remove(file_path)
-            print(file_path+" Completed Cleaning")
+            print(file_path+" Housekeeping has completed")
 
 for file_extracted in os.listdir(extracted_path):
     file_ebs = os.path.join(extracted_path, file_extracted)
@@ -22,4 +22,4 @@ for file_extracted in os.listdir(extracted_path):
     time_difference_days = time_difference / (24 * 60 * 60)
     if time_difference_days > 30:
         os.remove(file_ebs)
-        print(file_ebs+" Completed Cleaning")
+        print(file_ebs+" Housekeeping has completed")
